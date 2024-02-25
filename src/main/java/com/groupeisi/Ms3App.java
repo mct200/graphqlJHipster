245d1dea12b76da8954b77ab08332075bf18ec9b
coumba <coumbissthiam96@gmail.com>
2024-02-25 23:42:53 +0000

    first add

--- src/main/java/com/groupeisi/Ms3App.java
+++ src/main/java/com/groupeisi/Ms3App.java
@@ -15,10 +15,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.core.env.Environment;
 import tech.jhipster.config.DefaultProfileUtil;
 import tech.jhipster.config.JHipsterConstants;
 
+@EnableEurekaServer
 @SpringBootApplication
 @EnableConfigurationProperties({ LiquibaseProperties.class, ApplicationProperties.class })
 public class Ms3App {
